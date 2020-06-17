60d86bd59d83efe9d7df11c125de9c241d5c0b32
juliankarnik <julian.karnik@ecs-digital.co.uk>
2020-06-17 16:19:59 +0100

    feat: added basic mutation

--- cmd/main.go
+++ cmd/main.go
@@ -10,22 +10,13 @@ import (
 	"github.com/graphql-go/graphql"
 )
 
-func readdata(value string) string {
-	f, err := os.Open(value)
-	check(err)
-	reader1, err := ioutil.ReadAll(f)
-	check(err)
-	f.Close()
-	return string(reader1)
-}
-
 func main() {
 	fmt.Println("Launching the GraphQL server...")
 	err := os.Chdir("../data")
 	check(err)
 
 	// Schema
-	fields := graphql.Fields{
+	queryfields := graphql.Fields{
 		"hello": &graphql.Field{
 			Type: graphql.String,
 			Resolve: func(p graphql.ResolveParams) (interface{}, error) {
@@ -39,9 +30,36 @@ func main() {
 			},
 		},	}
 
-	rootQuery := graphql.ObjectConfig{Name: "RootQuery", Fields: fields}
-	schemaConfig := graphql.SchemaConfig{Query: graphql.NewObject(rootQuery)}
-	schema, err := graphql.NewSchema(schemaConfig)
+	mutationfields := graphql.Fields{
+		"createNewFile": &graphql.Field{
+			Type: graphql.String,
+			Args: graphql.FieldConfigArgument{
+				"filename": &graphql.ArgumentConfig{
+					Type: graphql.NewNonNull(graphql.String),
+				},
+				"text": &graphql.ArgumentConfig{
+					Type: graphql.String,
+				},
+			},
+			Resolve: func(params graphql.ResolveParams) (interface{} , error) {
+				filename, _ := params.Args["filename"].(string)
+				text, _ := params.Args["text"].(string)
+
+				// save text to filename here
+				writedata(filename, text)
+				return "ok", nil
+			},
+		},	}
+
+	rootQuery := graphql.ObjectConfig{Name: "RootQuery", Fields: queryfields}
+	rootMutation := graphql.NewObject(graphql.ObjectConfig{
+		Name: "RootMutation",
+		Fields:	mutationfields,
+	})
+	schema, err := graphql.NewSchema(graphql.SchemaConfig {
+		Query:		graphql.NewObject(rootQuery),
+		Mutation:	rootMutation,
+	})
 	if err != nil {
 		log.Fatalf("failed to create new schema, error: %v", err)
 	}
@@ -50,6 +68,7 @@ func main() {
 	query := `
 		{
 			hello
+			test
 		}		
 	`
 
@@ -61,20 +80,6 @@ func main() {
 	resultJSON, _ := json.Marshal(result)
 	fmt.Printf("%s \n", resultJSON) 
 
-	check(err)
-	h, err := os.Create("hello2")
-	check(err)
-	values := []byte("THis is a new string")
-	h.Write(values)
-	h.Close()
-	i, err := os.Open("hello2")
-	check(err)
-
-	reader2, err := ioutil.ReadAll(i)
-	i.Close()
-	fmt.Printf("hello2 holds values: %s\n", string(reader2))
-
-	fmt.Println("Finishing filepath access tests")
 	// set up the graphql server on http port 8080
 	http.HandleFunc("/graphql", func(write http.ResponseWriter, request *http.Request) {
 		result := executeQuery(request.URL.Query().Get("query"), schema)
@@ -82,6 +87,9 @@ func main() {
 	})
 
 	fmt.Println("Now GraphQL Server is running on port 8080")
+	http.ListenAndServe(":8080", nil)
+}
+
 // To test, run GraphiQL and visit endpoint http://localhost:8080/graphql?
 // with the following graphql query
 // query basichttptest {
@@ -95,13 +103,29 @@ func main() {
 // or
 // http://localhost:8080/graphql?query={hello%20test}
 
-	http.ListenAndServe(":8080", nil)
-}
-
 func check(e error) {
 	if e != nil {
 		panic(e)
 	}
+}
+
+func readdata(value string) string {
+	f, err := os.Open(value)
+	check(err)
+	reader1, err := ioutil.ReadAll(f)
+	check(err)
+	f.Close()
+	return string(reader1)
+}
+
+func writedata(filename string, text string) {
+	f, err := os.Create(filename)
+	check(err)
+	defer f.Close()
+
+	bytedata := []byte(text)
+	_, err = f.Write(bytedata)
+	check(err)
 }
 
 func executeQuery(query string, schema graphql.Schema) *graphql.Result {
