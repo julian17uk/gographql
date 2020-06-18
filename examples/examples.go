e47eb871c4ab51dea8842d090e3519cdbdc1e483
juliankarnik <julian.karnik@ecs-digital.co.uk>
2020-06-18 15:36:13 +0100

    fix: tidy file

--- examples/examples.go
+++ examples/examples.go
@@ -1,4 +1,5 @@
-// To test, run GraphiQL and visit endpoint http://localhost:8080/graphql?
+// To test, 
+// Run GraphiQL and visit endpoint http://localhost:8080/graphql?
 // with the following graphql query
 // query  {
 //	readFile(
@@ -11,19 +12,19 @@
 // 	  filename: "australia",
 // 	  text: "hello from australia",
 // 	)
-//   }
+// }
 //
 // mutation {
 // 	addToFile(
 // 	  filename: "australia",
 // 	  text: "major city is Melbourne",
 // 	)
-//   }
-
-// or using a browser with the following http request
+// }
+//
+// Or using a browser with the following http request
 // query
 // http://localhost:8080/graphql?query={readFile(filename:"hello")}
-// or
-// mutations
+//
+// mutation
 // http://[ipv6address]:8080/graphql?query=mutation{createNewFile(filename:"france", text:"major city is Sao Paulo")}
 // http://http://localhost:8080/graphql?query=mutation{addToFile(filename:%22france%22,text:%22a%20major%20city%20is%20bordeaux%22)}
